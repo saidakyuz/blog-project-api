6fb23e4030a5687f6a7749602dd0cadc3b4316db
Julia <77528734+juliagronau@users.noreply.github.com>
2021-03-24 17:09:32 +0100

    Add access control allow origin

--- index.js
+++ index.js
@@ -4,6 +4,16 @@ const port = process.env.PORT || 5000;
 const { getAllAuthors } = require("./controllers/authors");
 const { getAllPosts, getSinglePost } = require("./controllers/blogposts");
 
+// Add Access Control Allow Origin headers
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.get("/", (req, res) => {
   res.send(
     "<h1>This is the Sports Blog API</h1><br><h2>Those are the endpoints available:<h2/><p>To retrieve all authors: /authors</p><p>To retrieve all blogposts: /posts</p><p>To retrieve a single blogpost: /posts/:id</p>"
