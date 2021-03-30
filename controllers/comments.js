18aa3ed88dc234a1a457665a8733fe8c935c4af1
Julia <77528734+juliagronau@users.noreply.github.com>
2021-03-30 17:09:46 +0200

    create post method

--- controllers/comments.js
+++ controllers/comments.js
@@ -1,6 +1,5 @@
 const pool = require("../pg");
 
-
 const getCommentsOfPost = async (req, res) => {
   try {
     const { id } = req.params;
@@ -12,9 +11,22 @@ const getCommentsOfPost = async (req, res) => {
       res.status(404).json({ message: "This article has no comments yet" });
     res.json(results.rows[0]);
   } catch (error) {
-    res.status(500).json({error: error.message});
+    res.status(500).json({ error: error.message });
   }
 };
 
+const createPost = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, comment } = req.body;
+    const result = await pool.query(
+      "INSERT INTO comments(name, comment, post_id) VALUES($1, $2, $3) RETURNING *;",
+      [name, comment, id]
+    );
+    res.json(result.rows[0]);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
-module.exports = { getCommentsOfPost };
+module.exports = { getCommentsOfPost, createPost };
