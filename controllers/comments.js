e4567b0b21b553a7da6a74435bc9d91da2ffebbb
Matchu Pitchu <77695211+MatchuPitchu@users.noreply.github.com>
2021-03-31 15:05:11 +0200

    Solve error getCommentsOfPost

--- controllers/comments.js
+++ controllers/comments.js
@@ -4,12 +4,12 @@ export const getCommentsOfPost = async (req, res) => {
   try {
     const { id } = req.params;
     const results = await pool.query(
-      "SELECT * FROM comments WHERE post_id=$1;",
+      "SELECT * FROM comments WHERE post_id=$1 ORDER BY id DESC;",
       [id]
     );
     if (!results.rowCount)
       res.status(404).json({ message: "This article has no comments yet" });
-    res.json(results.rows[0]);
+    res.json(results.rows)
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
